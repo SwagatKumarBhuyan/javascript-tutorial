05429abafee2a13e6358712948406d218087d211
SwagatKumarBhuyan <124615710+SwagatKumarBhuyan@users.noreply.github.com>
2024-02-09 13:46:50 +0000

    operation complete

--- conversionOperation.js
+++ conversionOperation.js
@@ -18,9 +18,8 @@ true=>1 ; false=>0
 */
 
 let isLoggedIn="swagat"
-
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // notes
 // 1=>true; 0=>false 
@@ -39,3 +38,31 @@ let stringNumber=String(someNumber)
 let value =3
 let negValue= -value
 // console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1="hello"
+let str2="hitesh"
+let str3=str1+str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
