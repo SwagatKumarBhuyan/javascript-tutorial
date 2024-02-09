fc705b94d8f55e892bb57623841ec20ea1add6e6
SwagatKumarBhuyan <124615710+SwagatKumarBhuyan@users.noreply.github.com>
2024-02-09 13:19:42 +0000

    operation done

--- conversionOperation.js
+++ conversionOperation.js
@@ -27,7 +27,15 @@ console.log(booleanIsLoggedIn);
 // ""=>false 
 // "swagat"=>true 
 
+
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************OPERATION********88
+
+let value =3
+let negValue= -value
+// console.log(negValue);
