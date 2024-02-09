ca8373e3dd4d95ca9cf9fc8c311ca564af9ed6ef
Swagat Kumar Bhuyan <swagatkumarbhuyan167@gmail.com>
2024-02-09 18:38:27 +0530

    Add conversion files

---
+++ conversionOperation.js
@@ -0,0 +1,33 @@
+/*
+let score="swagat"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+*/
+
+ // Notes
+/*
+"33"=>33
+"33abc"=>NaN
+true=>1 ; false=>0
+*/
+
+let isLoggedIn="swagat"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// notes
+// 1=>true; 0=>false 
+// ""=>false 
+// "swagat"=>true 
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
