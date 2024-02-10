e479e14ff8594e22f594a1f657b34255d95f1c11
SwagatKumarBhuyan <124615710+SwagatKumarBhuyan@users.noreply.github.com>
2024-02-10 02:35:57 +0000

    string complete

---
+++ string.js
@@ -0,0 +1,33 @@
+const name = "swagat"
+const repoCount = 50
+
+// console.log(name+repoCount+"Value");  (old version coding)
+
+// console.log(`Hello my name is ${name.To} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('gamergeudu')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('e'));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,4);
+// console.log(anotherString);
+
+// const newStringOne = "   swagat    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://swagat.com/swagat%20bhuyan"
+// console.log(url.replace('%20','_'));
+// console.log(url.includes('swagat'));
+
+console.log(gameName.split('e'));
+
+
