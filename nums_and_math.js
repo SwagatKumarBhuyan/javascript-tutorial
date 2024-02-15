af84a499ac72bfef162d96426fb0fea965c29059
SwagatKumarBhuyan <124615710+SwagatKumarBhuyan@users.noreply.github.com>
2024-02-15 20:21:55 +0000

    math and num done

--- nums_and_math.js
+++ nums_and_math.js
@@ -1,5 +1,32 @@
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const anotherNumber = 123.8966
+// console.log(anotherNumber.toPrecision(3));
+
+const otherNumber = 1000000
+// console.log(otherNumber.toLocaleString('en-IN'));
+
+
+// ************* Maths **************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4,7,8,9,2));
+// console.log(Math.max(4,7,8,9,2));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
