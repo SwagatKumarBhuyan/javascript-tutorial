9b622ef15935fa9248bf6c16699fa80c3fd877ca
SwagatKumarBhuyan <124615710+SwagatKumarBhuyan@users.noreply.github.com>
2024-02-16 13:20:36 +0000

    array2

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,25 @@
+const marvel_heroes = ["thor","Ironman","Captin America"]
+const dc_heroes = ["Aquaman","Black widow","Ant-man"]
+
+// marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes);
+// console.log(marvel_heroes [3] [2]);
+
+// const all_heroes =marvel_heroes.concat(dc_heroes)
+// console.log(all_heroes);
+
+// const all_newheroes = [...marvel_heroes, ...dc_heroes]
+// console.log(all_newheroes);
+
+// const arr1 = [1,2,3,[4,5,6],7,[8,9,[1,2,3,[7,5]]]]
+// const arr2 = arr1.flat(Infinity)
+// console.log(arr2);
+
+// console.log(Array.isArray("swagat"));
+// console.log(Array.from("swagat"));
+// console.log(Array.from({name : "swagat"})); // For interview important
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
