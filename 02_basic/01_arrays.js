7b5100a9c2f019dc2f92b50e65ea48ff2259af71
SwagatKumarBhuyan <124615710+SwagatKumarBhuyan@users.noreply.github.com>
2024-02-16 12:52:42 +0000

    Array1

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,44 @@
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["Hanuman","Mahadev"]
+const myArr2 = new Array(1,2,3,4,5)
+// console.log(myArr[0]);
+
+// Arrays Method
+
+// myArr.push(6)
+// myArr.push(7)
+
+// myArr.pop()
+
+// myArr.unshift(9)
+
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(2));
+
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// slice , splice
+
+console.log("A" ,myArr);
+const mya1 = myArr.slice(1,3)
+console.log(mya1);
+
+console.log("B" , myArr);
+const mya2 = myArr.splice(1,3)
+console.log(mya2);
+
+console.log("C", myArr);
+
+// console.log(myArr);
+
+// ******** Notes *********
+
+// slice - range value not included in output
+// splice - range value included in output
+
+
+
