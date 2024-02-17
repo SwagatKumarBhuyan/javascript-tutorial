4d6fa342e45d35b9101f966bd0c8f12fbdae5d75
SwagatKumarBhuyan <124615710+SwagatKumarBhuyan@users.noreply.github.com>
2024-02-17 18:34:44 +0000

    scope done

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -1,5 +1,6 @@
 //  Scope are two types : Block , Global
 
+/*
 let a = 300
 {
     let a = 10
@@ -7,6 +8,52 @@ let a = 300
     console.log("Inner: ",a);
 }
 console.log("Outer :",a);
+*/
 
 //Notes------In the above whatever written in between {} these are block scope and outside written are global scope //
 
+/********* Nested Scope********/
+
+/*
+function one() 
+{
+   const username = "swagat"
+
+   function two(){
+    const website = "youtube"
+    console.log(username);
+   }
+//    console.log(website); // not executed bcz it outside of scope two
+
+two()
+
+}
+
+one()
+*/
+
+if (true) {
+    const username = "swagat"
+
+    if (username === "swagat") {
+        const website = "youtube"
+        console.log(username + website);
+    }
+    // console.log(website); // not executed bcz it outside of scope two 
+}
+// console.log(username); //not executed bcz it outside of scope one
+
+
+// ************* Interesting ***********
+
+addOne(5)          // Executed 
+function addOne(num){
+    return num+1
+}
+
+
+addTwo(5)          // Not executed bcz here we declare function by a object
+const addTwo = function(num)
+{
+    return num+2
+}
