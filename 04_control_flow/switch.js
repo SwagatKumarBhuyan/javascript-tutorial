c187f812546a148abda5aae6889e5009b8ece68f
SwagatKumarBhuyan <124615710+SwagatKumarBhuyan@users.noreply.github.com>
2024-02-18 06:47:42 +0000

    switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,48 @@
+// ******** Basic switch statement *********
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+
+/*
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    default:
+        console.log("april");
+        break;
+}
+*/
+
+
+const month = "february"
+
+switch (month) {
+    case "january":
+        console.log("1");
+        break;
+    case "february":
+        console.log("2");
+        break;
+    case "march":
+        console.log("3");
+        break;
+    default:
+        console.log("4");
+        break;
+}
