cb5deb0f11c2400557bc56cd573dfd06002c62a9
SwagatKumarBhuyan <124615710+SwagatKumarBhuyan@users.noreply.github.com>
2024-02-18 11:09:09 +0000

    while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,31 @@
+// ********** While loop *********
+
+/*
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+*/
+
+
+/*
+let myArray = ["Hanuman" , "Mahadev" , "Bishnu"]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+*/
+
+
+// ************ Do-While loop ***********
+
+let score = 11
+
+do {
+    console.log(`Value of score is ${score}`);
+    score++
+     
+} while (score <= 10);
+
