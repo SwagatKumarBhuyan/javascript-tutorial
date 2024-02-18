7fef6404620696f3ac90caaf8f30a2032e172fe0
SwagatKumarBhuyan <124615710+SwagatKumarBhuyan@users.noreply.github.com>
2024-02-18 19:39:14 +0000

    map

---
+++ 05_iteration/seven.js
@@ -0,0 +1,29 @@
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+/*
+const newNums = myNumbers.map( (num) => num + 10)
+console.log(newNums);
+*/
+
+/*
+const newNums = myNumbers.map( (num) => {return num + 10})
+console.log(newNums);
+*/
+
+/*
+const values =myNumbers.forEach( (items) => {
+    // console.log(items + 10);
+    return items + 10
+})
+console.log(values);
+*/
+
+/*
+const newNums = myNumbers
+                .map( (num) => num * 10)
+                .map( (num) => num + 5)
+                .filter( (num) => num >= 50)
+
+console.log(newNums);
+*/
+
