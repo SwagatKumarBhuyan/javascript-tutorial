a05977f7915f40de683fbece4fc13deec4b8e6d7
SwagatKumarBhuyan <124615710+SwagatKumarBhuyan@users.noreply.github.com>
2024-02-18 12:16:36 +0000

    for in loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,21 @@
+// *********** for in loop ************
+
+/*
+const myObj = {
+    js: "javasript",
+    pt: "python",
+    ja: "java",
+    re: "react"
+}
+
+for (const key in myObj) {
+    console.log(`${key} shorcut for ${myObj[key]}`);
+}
+*/
+
+
+const programming = ["js", "py", "ja", "re"]
+
+for (const key in programming) {
+   console.log(` ${programming[key]}`);
+}
